a62dfc371a8371d06f702ec0e3533c9e4866456e
Vishnu Narayanan <vishnu777aero@gmail.com>
2024-11-12 11:38:23 -0500

    add Jay's project, lab6 files Spring boot

--- C#/JayFirstProject/Controllers/HomeController.cs
+++ C#/JayFirstProject/Controllers/HomeController.cs
@@ -1,11 +1,14 @@
 using JayFirstProject.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using System.Diagnostics;
 
 namespace JayFirstProject.Controllers
 {
     public class HomeController : Controller
     {
+        private string connectionString = "Data Source=desktop-aiq6j2v;Initial Catalog=S3-CSharp;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -15,8 +18,34 @@ namespace JayFirstProject.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            List<Student> students = new List<Student>();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                string sqlQuery = "SELECT * FROM Students";
+                SqlCommand cmd = new SqlCommand(sqlQuery, con);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    students.Add(new Student
+                    {
+                        id = Convert.ToInt32(reader["id"]),
+                        name = reader["name"].ToString(),
+                        course = reader["course"].ToString(),
+                        year = Convert.ToInt32(reader["year"]),
+                        email = reader["email"].ToString()
+                    });
+                }
+
+                reader.Close();
+            }
+
+            return View(students);
         }
+
 
         public IActionResult Privacy()
         {

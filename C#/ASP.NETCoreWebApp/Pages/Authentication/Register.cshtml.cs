14d07d3fc7a03f5adb015fe6c7fe67fec70d1a6e
Vishnu Narayanan <vishnu777aero@gmail.com>
2025-02-19 20:02:17 -0500

    add login and registration pages

--- C#/ASP.NETCoreWebApp/Pages/Authentication/Register.cshtml.cs
+++ C#/ASP.NETCoreWebApp/Pages/Authentication/Register.cshtml.cs
@@ -1,20 +1,24 @@
+using ASP.NETCoreWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Data.SqlClient;
 
 namespace ASP.NETCoreWebApp.Pages.Authentication
 {
-    public class LoginModel : PageModel
+    public class RegisterModel : PageModel
     {
         public String username = "";
         public String password = "";
-        public String errorMessage = "";
+        public String email = "";
         public void OnPost()
         {
             try
             {
+
                 username = Request.Form["username"];
                 password = Request.Form["password"];
+                email = Request.Form["email"];
+
 
                 // save to DB
                 string connectionString = "Data Source=desktop-aiq6j2v;Initial Catalog=S3-CSharp;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
@@ -22,29 +26,29 @@ namespace ASP.NETCoreWebApp.Pages.Authentication
                 using (SqlConnection con = new SqlConnection(connectionString))
                 {
                     con.Open();
-                    String sql = "SELECT TOP (1) * FROM users " +
-                        "WHERE username = '" + username + "' AND password = '" + password + "';";
+                    String sql = "INSERT INTO users" +
+                        "(username, email, password)" +
+                        " VALUES" +
+                        "(@username, @email, @password);";
 
                     using (SqlCommand cmd = new SqlCommand(sql, con))
                     {
-                        SqlDataReader reader = cmd.ExecuteReader();
+                        cmd.Parameters.AddWithValue("@username", username);
+                        cmd.Parameters.AddWithValue("@email", email);
+                        cmd.Parameters.AddWithValue("@password", password);
 
-                        if (reader.Read())
-                        {
-                            Response.Redirect("/Clients/Index");
-                        }
-                        else {
-                            errorMessage = "Username or Password ERROR";
-                        }
+                        cmd.ExecuteNonQuery();
                     }
                 }
 
                 username = "";
+                email = "";
                 password = "";
+
+                Response.Redirect("/Clients/Index");
             }
             catch (Exception ex)
             {
-                errorMessage = ex.Message;
             }
         }
     }
